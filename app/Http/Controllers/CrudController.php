893caa7d1a4d72bd6df8bba61484ec8569ca7ce0
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-15 19:40:21 +0400

    Created controller

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -1,10 +1,30 @@
 <?php
-
+    
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use Exception;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Support\Facades\Http;
+  
 class CrudController extends Controller
 {
-    //
+    public function index()
+    {
+        $limiter = app(RateLimiter::class);
+        $actionKey = 'crud_name';
+        $threshold = 6;
+  
+        try {
+            if ($limiter->tooManyAttempts($actionKey, $threshold)) {
+                return $this->failOrFallback();
+            }
+            $req = Http::timeout(4)->get('https://domain.com/api');
+            $res = json_decode($req->body());
+    
+            dd($res);
+        } catch (Exception $exception) {
+            $limiter->hit($actionKey, Carbon::now()->addMinutes(16));
+            return $this->failOrFallback();
+        }
+    }
 }
