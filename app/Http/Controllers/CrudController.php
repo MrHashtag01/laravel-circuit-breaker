e3089ff002d01c311c48cd21abe63c4de037dfd6
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-15 20:54:02 +0400

    Index function 1

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -12,21 +12,9 @@ class CrudController extends Controller
 {
     public function index()
     {
-        $limiter = app(RateLimiter::class);
-        $actionKey = 'crud_name';
-        $threshold = 6;
+        $req = Http::get('https://domain.com/api');
+        $response = json_decode($req->body());
   
-        try {
-            if ($limiter->tooManyAttempts($actionKey, $threshold)) {
-                return $this->failOrFallback();
-            }
-            $req = Http::timeout(4)->get('https://domain.com/api');
-            $res = json_decode($req->body());
-    
-            dd($res);
-        } catch (Exception $exception) {
-            $limiter->hit($actionKey, Carbon::now()->addMinutes(16));
-            return $this->failOrFallback();
-        }
+        dd($response);
     }
 }
